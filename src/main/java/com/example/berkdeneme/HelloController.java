65bf9fd03d84e7b385dd1f2973c3fc915c5722fa
Berk <berkgguzel@gmail.com>
2024-05-03 00:51:55 +0300

    First commit by berk

---
+++ src/main/java/com/example/berkdeneme/HelloController.java
@@ -0,0 +1,44 @@
+package com.example.berkdeneme;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+
+public class HelloController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    private Label welcomeText;
+    @FXML
+    private Button startNewButton;
+    @FXML
+    private StackPane mainStackPane;
+
+
+    public void setBackButton(ActionEvent e) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
