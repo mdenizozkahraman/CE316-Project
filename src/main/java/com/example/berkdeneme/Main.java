0499ced6ec1a0cafeb801588b7e6b53879650558
M. Deniz Özkahraman <mehmetdenizozkahraman@outlook.com>
2024-05-03 22:40:37 +0300

    GUI

--- src/main/java/com/example/berkdeneme/Main.java
+++ src/main/java/com/example/berkdeneme/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1300, 800);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Integrated Environment System");
         stage.setScene(scene);
         stage.show();
