e676a26a6396be19ce32ade5473d4eb4972ef987
yigitcam1 <yiggit.cam@gmail.com>
2024-05-10 22:58:48 +0300

    Fxml and Controller Update

--- src/main/java/com/example/deneme/MainController.java
+++ src/main/java/com/example/deneme/MainController.java
@@ -8,6 +8,8 @@ import javafx.scene.control.Button;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 public class MainController implements Initializable {
     @FXML
@@ -29,11 +31,24 @@ public class MainController implements Initializable {
 
         helpButton.setOnAction(actionEvent -> {
             String helpTXT = " Integrated Assignment Environment ";
+
+            MainController.createHelp(helpTXT, "Help");
+
         });
 
 
         exitButton.setOnAction(actionEvent -> Platform.exit());
     }
+
+
+    public static void createHelp(String content, String header) {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("HELP");
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
 
 
 }
