a05d063ccb362cbc1f86c7929e0794fb5af0b550
Cerenengin15 <105562497+Cerenengin15@users.noreply.github.com>
2024-05-24 21:40:29 +0300

    new help button added to ResultController

--- src/main/java/com/example/deneme/ResultController.java
+++ src/main/java/com/example/deneme/ResultController.java
@@ -4,9 +4,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
@@ -14,6 +12,7 @@ import java.util.ResourceBundle;
 
 
 public class ResultController implements Initializable {
+    public ResultController() {}
     @FXML
     private TableView<ResultSceneClass> resultsTable;
 
@@ -25,6 +24,10 @@ public class ResultController implements Initializable {
 
     @FXML
     private TableColumn<ResultSceneClass, String> output;
+
+    @FXML
+    private Button helpButton2;
+
 
     @FXML
     private TableColumn<ResultSceneClass, String> result;
@@ -39,6 +42,21 @@ public class ResultController implements Initializable {
         result.setCellValueFactory(new PropertyValueFactory<>("result"));
 
         resultsTable.setItems(resultsList);
+
+        helpButton2.setOnAction(actionEvent -> {
+            String helpTXT = "The \"ID\" column, which is the first of the four columns you see in this window, gives the student number, the \"OUTPUT\" column gives the outputs of the code, the \"EXPECTED OUTCOME\" column gives the actual output that the code should give, and the \"RESULT\" column gives the result of comparing the expected output with the student output." +
+                     "If two values are the same, it displays \"true\"; if they are different, it displays \"false\".";
+
+            ResultController.help(helpTXT, "Help");
+
+        });
+    }
+    public static void help(String content, String header) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("HELP");
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        alert.showAndWait();
     }
 
     public void addResult(String path, String output, String expectedOutput, String result) {
